142782a10dcf19b153b9a776a5036b7bb3128155
Renata <110370964+An-Renata@users.noreply.github.com>
2024-03-16 17:50:57 +0200

    Cart description
    Correct the on hover state the description of the checkout, no not turn it off if hover is on the component itself

--- src/ui/UserInfo.jsx
+++ src/ui/UserInfo.jsx
@@ -1,8 +1,9 @@
 import styled, { css } from "styled-components";
 import Cart from "./Cart";
-import { HiOutlineShoppingCart } from "react-icons/hi";
+import { HiOutlineShoppingCart, HiOutlineTrash } from "react-icons/hi";
 import { useProduct } from "../context/ProductContext";
-import { useRef, useState } from "react";
+import { useRef } from "react";
+import Button from "./Button";
 
 const StyledUser = styled.div`
   display: flex;
@@ -10,7 +11,6 @@ const StyledUser = styled.div`
   justify-content: center;
   align-items: center;
   gap: 40px;
-  position: relative;
 `;
 
 const AvatarImg = styled.img`
@@ -29,12 +29,39 @@ const AvatarImg = styled.img`
 const CartSummary = styled.div`
   position: absolute;
   color: var(--color-dark-grayish-blue);
-  width: 200px;
-  border: 1px solid red;
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  background-color: var(--color-white);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+
+  border-radius: 10px;
+  & h2 {
+    border-bottom: 1px solid var(--color-grayish-blue);
+    font-size: 1.4rem;
+    padding-bottom: 20px;
+  }
+
+  & div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  & div > div {
+    display: flex;
+    flex-direction: column;
+    font-size: 1rem;
+    align-items: flex-start;
+  }
+
   ${(props) =>
-    props?.pos?.y &&
+    props?.pos &&
     css`
-      top: ${props.pos.y + 80}px;
+      top: ${props.pos.y + 50}px;
+      left: ${props.pos.x - 100}px;
     `};
 `;
 
@@ -66,7 +93,27 @@ function UserInfo() {
           alt="avatar"
         ></AvatarImg>
       </StyledUser>
-      {isHoverCart && <CartSummary pos={pos}>Cart is empty</CartSummary>}
+      {isHoverCart && (
+        <CartSummary pos={pos}>
+          <h2>Cart</h2>
+
+          <div>
+            <img
+              src="../public/images/image-product-1-thumbnail.jpg"
+              style={{ width: "10%", height: "10%", borderRadius: "5px" }}
+            />
+            <div>
+              <h3>Fall Limited Edition Sneakers</h3>
+              <p>$125.00 x 3</p>
+              <p>$375.00</p>
+            </div>
+            <Button>
+              <HiOutlineTrash size={15} />
+            </Button>
+          </div>
+          <Button type="addCart">Checkout</Button>
+        </CartSummary>
+      )}
     </div>
   );
 }
