59f3b5c78c73099191fc5f5d179f6e4d3788cdd0
Renata <110370964+An-Renata@users.noreply.github.com>
2024-03-22 10:58:46 +0200

    Image container
    Implemented Image Container with  arrows to navigate through the images.

--- src/ui/ImagesComponent.jsx
+++ src/ui/ImagesComponent.jsx
@@ -2,7 +2,11 @@ import styled, { css } from "styled-components";
 import MainImage from "./MainImage";
 import { imageData } from "../data/imageData";
 import { useState } from "react";
-import { HiOutlineArrowCircleLeft } from "react-icons/hi";
+import {
+  HiOutlineArrowCircleLeft,
+  HiOutlineArrowLeft,
+  HiOutlineArrowRight,
+} from "react-icons/hi";
 
 const StyledImages = styled.div`
   padding: 0 80px;
@@ -18,6 +22,7 @@ const Thumbnails = styled.img`
   width: 20%;
   border-radius: 10px;
   cursor: pointer;
+
   ${({ selected }) =>
     selected &&
     css`
@@ -29,12 +34,75 @@ const Thumbnails = styled.img`
   }
 `;
 
+const MainImageContainer = styled.div`
+  position: relative;
+
+  & span {
+    padding: 10px;
+    color: var(--color-primary-orange);
+    border-radius: 50%;
+    background-color: var(--color-white);
+    opacity: 0.5;
+    transition: all 0.2s;
+    display: flex;
+  }
+
+  & span:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+
+  & span:nth-child(1) {
+    position: absolute;
+    top: 50%;
+    left: 5px;
+    transform: translateY(-50%);
+  }
+
+  /* second  span element */
+  & span:nth-child(2) {
+    position: absolute;
+    top: 50%;
+    right: 5px;
+    transform: translateY(-50%);
+  }
+`;
 function ImagesComponent() {
   const [selectedImage, setSelectedImage] = useState(imageData[0]);
 
+  function handleClickLeft() {
+    // Get the index of the selected image
+    const index = imageData.indexOf(selectedImage);
+    // If the index is set to 0, and user clicks again, show the last image
+    if (index === 0) {
+      setSelectedImage(imageData[imageData.length - 1]);
+    } else {
+      setSelectedImage(imageData[index - 1]);
+    }
+  }
+
+  function handleClickRight() {
+    const index = imageData.indexOf(selectedImage);
+    if (index === imageData.length - 1) {
+      setSelectedImage(imageData[0]);
+    } else {
+      setSelectedImage(imageData[index + 1]);
+    }
+  }
   return (
     <StyledImages>
-      <MainImage selectedImage={selectedImage} />
+      <MainImageContainer>
+        {/* Arrows */}
+        <span onClick={handleClickLeft}>
+          <HiOutlineArrowLeft size={15} />
+        </span>
+        <span onClick={handleClickRight}>
+          <HiOutlineArrowRight size={15} />
+        </span>
+        {/* .............. */}
+
+        <MainImage selectedImage={selectedImage} />
+      </MainImageContainer>
       <ImageThumbnailsContainer>
         {/* Images thumbnails */}
         {imageData?.map((image) => (
